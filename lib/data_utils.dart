af1a5128464ce52bdf645e4818719191e3777bb5
Miguel Gómez Frieyro <mgf00050@red.ujaen.es>
2025-01-30 18:55:11 +0100

    Punto Inicial 1

---
+++ lib/data_utils.dart
@@ -0,0 +1,9 @@
+abstract class DataUtils {
+  static String getUserImage(String username) {
+    return _getImageUrl(username);
+  }
+
+  static String getChannelImage() => _getImageUrl('');
+
+  static String _getImageUrl(String value) => 'https://ui-avatars.com/api/?name=$value&backgroung=random';
+}
