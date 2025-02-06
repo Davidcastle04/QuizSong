af82bf9e37c14806b075dbd02e637ea16ca2b573
David Castillo Serrano <dcs00037@red.ujaen.es>
2025-02-06 12:13:37 +0100

    Arreglo de errores de compilación

--- lib/data_utils.dart
+++
@@ -1,9 +0,0 @@
-abstract class DataUtils {
-  static String getUserImage(String username) {
-    return _getImageUrl(username);
-  }
-
-  static String getChannelImage() => _getImageUrl('');
-
-  static String _getImageUrl(String value) => 'https://ui-avatars.com/api/?name=$value&backgroung=random';
-}
--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
+import 'package:quizsong/ui/screens/splash/splash_screen.dart';
 
 
 void main() async {
